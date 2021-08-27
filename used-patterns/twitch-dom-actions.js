362d0f36e5eb041dfcfbdbdfcb39ca8e8bdb3f9f
HenryLimaBR <djfilipebr@gmail.com>
2021-08-27 04:59:16 -0300

    Update Some Patterns to Implement

--- used-patterns/twitch-dom-actions.js
+++ used-patterns/twitch-dom-actions.js
@@ -1,6 +1,5 @@
 // Carousel Visible Username
 document.querySelector('div.carousel-metadata').children[0].children[1].children[0].href
-
 // Carousel Not Visible Username
 document.querySelector('p[data-test-selector="stream-info-card-component__title"]').children[0].href
 
@@ -11,14 +10,5 @@ document.querySelector('button[data-test-selector="chat-viewers-list__button"][d
 document.querySelector('button[data-test-selector="user-details-actions-ban-button"]')
 
 // Input to filter user 
+// React Component with private 'input' event :/
 document.querySelector('input[type="search"][name="viewers-filter"]')
-
-/** @type {HTMLInputElement} */
-const search = document.querySelector('input[type="search"][name="viewers-filter"]')
-search.value = 'henry'
-
-// Not Working Yet (searching for a solution :/)
-search.dispatchEvent(new Event('input', {
-  bubbles: true,
-  cancelable: true
-}))
