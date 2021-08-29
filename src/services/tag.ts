84bd6be269006cd255000fae0346b7ee85e8a87e
HenryLimaBR <djfilipebr@gmail.com>
2021-08-29 01:45:25 -0300

    Fixed Trusted, Unknown and Viewer in same User

--- src/services/tag.ts
+++ src/services/tag.ts
@@ -12,27 +12,42 @@ export function addTags(
     username,
     badges: [{ source: 'Twitch', type: 'streamer' }]
   })))
+
   list.push(...TwitchTMIData.chatters.moderators.map((username): ListUser => ({
     username,
     badges: [{ source: 'Twitch', type: 'moderator' }]
   })))
+
   list.push(...TwitchTMIData.chatters.vips.map((username): ListUser => ({
     username,
     badges: [{ source: 'Twitch', type: 'vip' }]
   })))
+
   list.push(...TwitchTMIData.chatters.viewers.map((username): ListUser => ({
     username,
     badges: [{ source: 'Twitch', type: 'viewer' }]
   })))
+
   const unknown = list.map((user) => {
+    const source = 'TwitchInsights'
     if (unknown_list.includes(user.username)) {
-      user.badges.push({ source: 'Twitch Insights', type: 'unknown_bot' })
+      if (user.badges[user.badges.length - 1].type === 'viewer') {
+        user.badges[user.badges.length - 1] = { source, type: 'unknown_bot' }
+        return user
+      }
+      user.badges.push({ source, type: 'unknown_bot' })
     }
     return user
   })
+  
   const trusted = unknown.map((user) => {
+    const source = 'Twitch Bot Highlight'
     if (trusted_list.includes(user.username)) {
-      user.badges.push({ source: 'Twitch Bot Highlight', type: 'trusted_bot' })
+      if (user.badges[user.badges.length - 1].type === 'unknown_bot') {
+        user.badges[user.badges.length - 1] = { source, type: 'trusted_bot' }
+        return user
+      }
+      user.badges.push({ source, type: 'trusted_bot' })
     }
     return user
   })
