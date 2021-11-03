812fa205415483364cac4c84a0c89b7c276c26b6
HenryLimaBR <djfilipebr@gmail.com>
2021-11-03 03:11:24 -0300

    UI Color Updates

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,21 +1,30 @@
 import React from 'react'
 import { ListItemProps } from '../../../typings/rc'
+import { capitalizeAll } from '../../utils/string'
 
 import style from './style.module.scss'
 
-const color = {
-  'streamer': '#0bf',
-  'moderator': '#08f',
-  'vip': '#ff0',
-  'viewer': '#fff',
-  'trusted_bot': '#0f8',
-  'unknown_bot': '#f80'
-}
+import { badge_colors as colors } from '../../styles/colors'
 
 const ListItem: React.FC<ListItemProps> = ({ user }) => {
+  const color = colors[user.badges[user.badges.length - 1].type]
   return (
-    <li className={style.container} style={{ color: color[user.badges[user.badges.length - 1].type] }}>
-      <p title={user.badges[user.badges.length - 1].type.toUpperCase().replace(/_/g, ' ')} style={{ userSelect: 'none' }}>{user.username}</p>
+    <li className={style.container}>
+      <p
+        className={style.username}
+        style={{ color }}
+        title={user.username}
+      >{user.username.slice(0, 16)}</p>
+
+      <div className={style.badge_container}>
+        {
+          user.badges.map((badge) => {
+            const title = `${capitalizeAll(badge.type, '_', ' ')} - ${badge.source}`
+            const backgroundColor = colors[badge.type]
+            return (<div className={style.badge} style={{ backgroundColor }} title={title}></div>)
+          })
+        }
+      </div>
     </li>
   )
 }
---
+++ src/styles/colors.ts
@@ -0,0 +1,8 @@
+export const badge_colors = {
+  'streamer': '#0df',
+  'moderator': '#08f',
+  'vip': '#ff0',
+  'viewer': '#fff',
+  'trusted_bot': '#0f8',
+  'unknown_bot': '#f80'
+}
